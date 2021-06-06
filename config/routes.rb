e81ccfb4bb798da0d70178367453114b31159618
takasugiren <bucyou16528080@gmail.com>
2021-06-06 11:57:39 +0000

    [Update]

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
   root 'homes#top'
   get 'home/about' => 'homes#about'
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
 end
