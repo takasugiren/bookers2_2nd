e6a6c343b8d25db4c1f607c3fe80f7f914ed29e3
takasugiren <bucyou16528080@gmail.com>
2021-06-08 02:43:52 +0000

    [Update]2

---
+++ app/controllers/searchs_controller.rb
@@ -0,0 +1,15 @@
+class SearchsController < ApplicationController
+  before_action :authenticate_user!
+
+
+  def search
+    @range = params[:range]
+    @word = params["word"]
+
+    if @range == "User"
+      @users = User.looks(params[:search], params[:word])
+    else
+      @books = Book.looks(params[:search], params[:word])
+    end
+  end
+end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,19 @@ class Book < ApplicationRecord
 	belongs_to :user
 	has_many :favorites, dependent: :destroy
 	has_many :book_comments, dependent: :destroy
+	def self.looks(search, word)
+    if search == "perfect_match"
+      @book = Book.where("title LIKE?","#{word}")
+    elsif search == "forward_match"
+      @book = Book.where("title LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @book = Book.where("title LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @book = Book.where("title LIKE?","%#{word}%")
+    else
+      @book = Book.all
+    end
+  end
 
 	validates :title, presence: true
 	validates :body, presence: true, length: {maximum: 200}
@@ -9,4 +22,6 @@ class Book < ApplicationRecord
 	def favorited_by?(user)
 		favorites.where(user_id: user.id).exists?
 	end
+
+
 end
